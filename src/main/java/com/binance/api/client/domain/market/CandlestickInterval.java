c922d0e05ea568199c6c61d17b84a979678c3079
Teije <iteije@outlook.com>
2022-01-29 00:13:41 +0100

    Add number of minutes to candlestick interval

--- src/main/java/com/binance/api/client/domain/market/CandlestickInterval.java
+++ src/main/java/com/binance/api/client/domain/market/CandlestickInterval.java
@@ -8,30 +8,35 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public enum CandlestickInterval {
-  ONE_MINUTE("1m"),
-  THREE_MINUTES("3m"),
-  FIVE_MINUTES("5m"),
-  FIFTEEN_MINUTES("15m"),
-  HALF_HOURLY("30m"),
-  HOURLY("1h"),
-  TWO_HOURLY("2h"),
-  FOUR_HOURLY("4h"),
-  SIX_HOURLY("6h"),
-  EIGHT_HOURLY("8h"),
-  TWELVE_HOURLY("12h"),
-  DAILY("1d"),
-  THREE_DAILY("3d"),
-  WEEKLY("1w"),
-  MONTHLY("1M");
+  ONE_MINUTE("1m", 1),
+  THREE_MINUTES("3m", 3),
+  FIVE_MINUTES("5m", 5),
+  FIFTEEN_MINUTES("15m", 15),
+  HALF_HOURLY("30m", 30),
+  HOURLY("1h", 60),
+  TWO_HOURLY("2h", 120),
+  FOUR_HOURLY("4h", 240),
+  SIX_HOURLY("6h", 360),
+  EIGHT_HOURLY("8h", 480),
+  TWELVE_HOURLY("12h", 720),
+  DAILY("1d", 1440),
+  THREE_DAILY("3d", 4320),
+  WEEKLY("1w", 10080)
+  ;
 
   private final String intervalId;
+  private final long minutes;
 
-  CandlestickInterval(String intervalId) {
+  CandlestickInterval(String intervalId, long minutes) {
     this.intervalId = intervalId;
+    this.minutes = minutes;
   }
 
   public String getIntervalId() {
     return intervalId;
   }
 
+  public long getMinutes() {
+    return minutes;
+  }
 }
